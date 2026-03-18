f1a29ae6edc30515dfdaa42ea20eaa2e49a0ecdf
Dmitriy <babygreenyoda@yandex.ru>
2026-03-18 20:43:51 +0800

    Refoctoring: class Database to more universal using

--- SQLliteTask/Nodes/database_node.py
+++ SQLliteTask/Nodes/database_node.py
@@ -1,40 +1,22 @@
 import sqlite3
 
-class DatabaseNode:
-    def __init__(self,db_name):
-        self.db_name = db_name
-        self.connection = sqlite3.connect(db_name)
-        self.cursor = self.connection.cursor()
-        self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS `job_titles` (
-             `id_job_title` integer primary key NOT NULL UNIQUE,
-            `name` TEXT NOT NULL
-);
-""")
+class Database:
 
-        self.cursor.execute("""
-CREATE TABLE IF NOT EXISTS `employees` (
-    `id` integer primary key NOT NULL UNIQUE,
-    `surname` TEXT NOT NULL,
-    `name` TEXT NOT NULL,
-    `id_job_title` INTEGER NOT NULL,
-    FOREIGN KEY(`id_job_title`) REFERENCES `job_titles`(`id_job_title`)
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
 
-                    );
-""")
-    def insert_job_titles(self, data):
-        self.cursor.executemany(
-            "INSERT OR IGNORE INTO job_titles (id_job_title, name) VALUES (?, ?)",
-            data
-        )
-        self.connection.commit()
+    def query(self, sql, params=()):
+        cur = self.conn.cursor()
+        cur.execute(sql, params)
+        return cur.fetchall()
 
-    def insert_employees(self, data):
-        self.cursor.executemany(
-            "INSERT OR IGNORE INTO employees (id, surname, name, id_job_title) VALUES (?, ?, ?, ?)",
-            data
-        )
-        self.connection.commit()
-        self.connection.commit()
+    def execute(self, sql: str, params=()):
+        cur = self.conn.cursor()
+        cur.execute(sql, params)
+        self.conn.commit()
+    def executemany(self, sql : str, params=()):
+        cur =self.conn.cursor()
+        cur.executemany(sql,params)
+        self.conn.commit()
     def close(self):
-        self.connection.close()
+        self.conn.close()

3ac61242d8a7b340191866d507c50f4c103b31ef
Dmitriy <babygreenyoda@yandex.ru>
2026-03-19 18:50:47 +0800

    Add: the ability to read CSV and Excel

--- SQLliteTask/Nodes/database_node.py
+++ SQLliteTask/Nodes/database_node.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+import pandas as pd
+from datetime import datetime
 class Database:
 
     def __init__(self, db):
@@ -18,5 +19,13 @@ class Database:
         cur =self.conn.cursor()
         cur.executemany(sql,params)
         self.conn.commit()
+    def read_xls(self, file_path:str,sheet_name:str):
+        df = pd.read_excel(file_path, sheet_name=sheet_name)
+        return df
+    def read_csv(self,file_path:str):
+        cf =pd.read_csv(file_path)
+
+
+
     def close(self):
         self.conn.close()
